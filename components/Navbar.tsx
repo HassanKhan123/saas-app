d97bfa12ae7d65d2a212093d8129105e44672c34
HassanKhan123 <hk260280@gmail.com>
2025-10-03 19:46:01 +0200

    added: clerk auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "./NavItems";
-// import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -13,14 +13,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        {/* <SignedOut>
+        <SignedOut>
           <SignInButton>
             <button className="btn-signin">Sign In</button>
           </SignInButton>
         </SignedOut>
         <SignedIn>
           <UserButton />
-        </SignedIn> */}
+        </SignedIn>
       </div>
     </nav>
   );
