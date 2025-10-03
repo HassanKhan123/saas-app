64263732a7ab341ce875dad51fbd3fc37fdbe101
HassanKhan123 <hk260280@gmail.com>
2025-10-03 18:52:39 +0200

    added: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,56 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/CTA";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Hello</h1>
-      <Button>Click</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        {/* {companions.map((companion) => ( */}
+        <CompanionCard
+          id="1"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          duration={45}
+          color="#E5D0FF"
+          subject="science"
+          bookmarked={false}
+        />
+        <CompanionCard
+          id="2"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          duration={30}
+          color="#FFDA6E"
+          subject="maths"
+          bookmarked
+        />
+        <CompanionCard
+          id="3"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          duration={30}
+          color="#BDE7FF"
+          subject="language"
+          bookmarked
+        />
+        {/* ))} */}
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
   );
 };
 
